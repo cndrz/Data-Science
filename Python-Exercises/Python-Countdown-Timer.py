4752bcfdc44794a10a79cd7a4c2065595ab63a41
cndrz <135137323+cndrz@users.noreply.github.com>
2023-09-03 07:40:56 +0800

    ---

--- Python-Exercises/Python-Countdown-Timer.py
+++ Python-Exercises/Python-Countdown-Timer.py
@@ -2,13 +2,11 @@ import time
 def countdown(time_and_seconds):
     while time_and_seconds:
         minutes, seconds = divmod(time_and_seconds, 60)
-        time_format =  "{:02d}: {:02d}".format(minutes,seconds)
+        time_format =  "{:02d} : {:02d}".format(minutes,seconds)
         print(time_format, end = "\r")
         time.sleep(1)
         time_and_seconds -= 1
 
-print("Countdown finished.")
+countdown(10) #Enter amount in seconds!!!
 
-countdown(0) #Enter amount in seconds!!!
-
-    
+print("Timer finished.")
