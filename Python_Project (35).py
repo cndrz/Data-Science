0ab63172cab6831eaf973f64e65c938006e45af4
cndrz <135137323+cndrz@users.noreply.github.com>
2023-12-14 09:38:00 +0800

    ---

---
+++ Python_Project (35).py
@@ -0,0 +1,39 @@
+import csv
+
+class student_sign_in:
+    
+    def __init__(x):
+        x.student_name = None
+        x.student_number = None
+    
+    def welcome(x):
+
+        print(f"Welcome To Topaz University, {x.student_name}.")
+
+    def sign_in(x):
+
+        x.student_name = input("Enter Student Name: ")
+        if any(character.isdigit() for character in x.student_name):
+            return "Your Name Should Not Contain Any Number. Sign-in Failed."
+
+        x.student_number= int(input("Enter Student Number: "))
+        if any(character.isalpha() for character in x.student_name): 
+            return "Your Student Number Should Not Contain Any Letter. Sign-in Failed."
+
+    def file(x):
+
+        with open("Student Logbook", "a", newline = "") as student_file:
+            w = csv.writer(student_file)
+            w.writerow(f"Student Name: {x.student_name}")
+            w.writerow(f"Student Number: {x.student_number}")
+            w.writerow(" ")
+    
+
+student = student_sign_in()
+
+student.sign_in()
+student.file()
+student.welcome()
+
+
+
