76e7fdd6b1ca479101181c5a2e64d43130b9e5f6
cndrz <135137323+cndrz@users.noreply.github.com>
2024-01-30 21:33:55 +0800

    ---

--- Python Project (40).py
+++ Python Project (40).py
@@ -1,4 +1,12 @@
-with open("Python Projects/poem_folder/poem.txt") as poem:
-    for line in poem:
-        print(line)
-        
+import random
+
+def open_file(numbers):
+    with open("number_list.txt", "w") as file:
+        for number in numbers:
+            file.write(f"{str(number)}\n")
+
+
+number_list = [random.randint(1, 50) for _ in range(5)]
+
+
+open_file(number_list)
