4fa07138c67e2ef2d3b35f8d4d2f8f874392f8a5
cndrz <135137323+cndrz@users.noreply.github.com>
2024-02-02 13:57:52 +0800

    ---

--- Python Project (41).py
+++ Python Project (41).py
@@ -1,12 +1,14 @@
 import os
 import csv
+from datetime import datetime
 class logbook:
 
     def __init__(x):
-
+    
         x.name = None
         x.number = None
-        x.time = None
+        x.time = datetime.now().strftime("%H:%M:%S")
+        x.date = datetime.now().strftime("%m-%d-%Y")
 
     def Employee_log(x):
         global log_file
@@ -14,17 +16,26 @@ class logbook:
             writer = csv.writer(log_file)
             writer.writerow([f"Employee Name: {x.name}"])
             writer.writerow([f"Employee Number: {x.number}"])
+            writer.writerow([f"Date: {x.date}"])
             writer.writerow([f"Time In: {x.time}"])
+            writer.writerow(" ")
 
 
     def login(x):
-        x.name = input("Enter Employee Name: ")
-        if any(character.isdigit() for character in x.name):
-            return "Your Name Should Not Contain Any Number. Try Again."
+        while True:
+            try:
+                x.name = input("Enter Employee Name: ")
+                if any(character.isdigit() for character in x.name):
+                    raise ValueError("Your Name Should Not Contain Any Number. Try Again.")
 
-        x.number = input("Enter Employee Number: ")
-        if any(num.isalpha() for num in x.number):
-            return "Your Employee Number Should Not Contain Any Letter. Try Again."
+                x.number = input("Enter Employee Number: ")
+                if any(num.isalpha() for num in x.number):
+                    raise ValueError("Your Employee Number Should Not Contain Any Letter. Try Again.")
+
+                break
+
+            except ValueError as error:
+                print(error)
         
 
 log = logbook()
