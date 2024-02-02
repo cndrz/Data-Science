5bc3d99b5a4cd4ee02e94b92c5d1189427ed9938
cndrz <135137323+cndrz@users.noreply.github.com>
2024-02-02 12:47:01 +0800

    ---

--- Python Project (38).py
+++ Python Project (38).py
@@ -40,7 +40,6 @@ class account:
 
 user_account = account()
 user_account.sign_up()
-user_account.account_details
 user_account.file()
 
          
--- Python Project (41).py
+++ Python Project (41).py
@@ -1,7 +1,32 @@
-name_dictionary = {
-    "Employee Number": "10001",
-    "Employee Name": "Ford Masawi",
-    "Employee Position": "Branch Manager",
-}
+import os
+import csv
+class logbook:
 
-print(name_dictionary)
+    def __init__(x):
+
+        x.name = None
+        x.number = None
+        x.time = None
+
+    def Employee_log(x):
+        global log_file
+        with open("Employee Logbook.csv", "a", newline = "") as log_file:
+            writer = csv.writer(log_file)
+            writer.writerow([f"Employee Name: {x.name}"])
+            writer.writerow([f"Employee Number: {x.number}"])
+            writer.writerow([f"Time In: {x.time}"])
+
+
+    def login(x):
+        x.name = input("Enter Employee Name: ")
+        if any(character.isdigit() for character in x.name):
+            return "Your Name Should Not Contain Any Number. Try Again."
+
+        x.number = input("Enter Employee Number: ")
+        if any(num.isalpha() for num in x.number):
+            return "Your Employee Number Should Not Contain Any Letter. Try Again."
+        
+
+log = logbook()
+log.login()
+log.Employee_log()
