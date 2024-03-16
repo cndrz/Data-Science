a771d164f8d7fe5014f89ccbe23333691d2791d7
cndrz <xxjmdelacruzxx@gmail.com>
2024-03-16 20:55:08 +0800

    TBC

---
+++ School Library Login System/main.py
@@ -0,0 +1,16 @@
+import os
+
+class Student:
+
+
+    def __init__(self):
+
+        self.student_name = None
+        self.student_number = None
+
+    def student_logs():
+        pass
+
+
+        
+
