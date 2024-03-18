92d9c5465234af06a08a733fd97faff10071de92
cndrz <xxjmdelacruzxx@gmail.com>
2024-03-18 21:14:55 +0800

    Organized the file and added a choice method

---
+++ BRC Dossier/BRC_main.py
@@ -0,0 +1,28 @@
+print("--- Welcome to the BRC Dossier ---")
+print("A. BRC Sectors")
+print("B. Objects of Power")
+print("C. Altered World Events")
+print("D. What is the BRC Dossier?")
+choice = input("Choose one: ")
+
+match choice:
+   
+    case "A":
+        # BRC Sectors 
+        pass
+     
+    case "B":
+        # Objects of Power
+        pass
+         
+    case "C":
+        # Altered World Events
+        pass
+
+    case "D":
+        # What is the BRC Dossier?
+        pass
+
+    case _  : 
+        # Prompt for input re-entry
+        pass
