3aa7540fa9f7ed1556ba2cdee7a6aa38dfba029e
cndrz <xxjmdelacruzxx@gmail.com>
2024-03-20 07:55:26 +0800

    Added directories in the file read sector

--- BRC Dossier/SECTORS.py
+++ BRC Dossier/SECTORS.py
@@ -12,31 +12,31 @@ def sector_select():
             
             match choice:
                 case "A":
-                     with open(r"C:\Users\admin\Desktop\Python\BRC Dossier\BRC Sectors", "r") as Executive_Sector:
+                     with open(r"C:\Users\admin\Desktop\Python\BRC Dossier\BRC Sectors\Executive Sector.txt", "r") as Executive_Sector:
                         file_1 = Executive_Sector.read()
                         print(file_1)
                         break
 
                 case "B":
-                     with open("file_A.txt", "r") as Containment_Sector:
+                     with open(r"C:\Users\admin\Desktop\Python\BRC Dossier\BRC Sectors\Containment Sector.txt", "r") as Containment_Sector:
                         file_2 = Containment_Sector.read()
                         print(file_2)
                         break
 
                 case "C":
-                     with open("file_A.txt", "r") as Investigations_Sector:
+                     with open(r"C:\Users\admin\Desktop\Python\BRC Dossier\BRC Sectors\Investigations Sector.txt", "r") as Investigations_Sector:
                         file_3 = Investigations_Sector.read()
                         print(file_3)
                         break
                 
                 case "D":
-                     with open("file_A.txt", "r") as Maintenance_Sector:
+                     with open(r"C:\Users\admin\Desktop\Python\BRC Dossier\BRC Sectors\Maintenance Sector.txt", "r") as Maintenance_Sector:
                         file_4 = Maintenance_Sector.read()
                         print(file_4)
                         break
 
                 case "E":
-                     with open("file_A.txt", "r") as Research_Sector:
+                     with open(r"C:\Users\admin\Desktop\Python\BRC Dossier\BRC Sectors\Research Sector.txt", "r") as Research_Sector:
                         file_5 = Research_Sector.read()
                         print(file_5)
                         break
